7a4f88888326cbc8a9745e6b0e5e86d5b017a54d
Fran <micalleffran@gmail.com>
2019-05-02 22:14:57 +0200

    removed py workshops as theyre in another repo

--- PythonWorkshops/ex2.py
+++
@@ -1,9 +0,0 @@
-#A comment, this is so you can read your program later
-#Lol
-
-print("I could have code like this.") #caw
-
-##bla
-#print("this wont run")
-
-print("this will.")
--- PythonWorkshops/ex34.py
+++
@@ -1,4 +0,0 @@
-animals = ["bear", "python", "bird", "penguin", "horse"]
-
-print(f"The first animal is {animals[0]}")
-print(f"The last animal is {animals[len(animals)-1]}")
